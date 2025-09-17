f84bb4030d1987901980a0e42e730f9f8a0e2622
ranjith2007 <ranjithkumar0799@gmail.com>
2025-09-17 20:33:23 +0530

    added conversation script

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -5,3 +5,6 @@ echo "Manoj:: Hi Ranjith, I'm fine what about you?"
 echo "Ranjith:: Fine, what's going on?"
 echo "Manoj:: I'm practicing shell script, What about you?"
 echo "Ranjith:: I'm also practicing shell script"
+
+
+#Next 03-variables. Instead of mentioning name in the conversation. Using variables we can easily pass from outside through arguments.
