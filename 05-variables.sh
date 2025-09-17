dc5fc5087aba9d0c92fa1ee31aefa3e61b7fa696
ranjith2007 <ranjithkumar0799@gmail.com>
2025-09-17 21:27:03 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,12 +3,12 @@
 
 echo "Please enter your username::"
 
-read  USERNAME #takes input into username variable / -s is for hiding(You can hide while entering data)
+read  -s USERNAME #takes input into username variable / -s is for hiding(You can hide while entering data)
 
 echo "Username entered is: $USERNAME"
 
 echo "Please enter your password::"
 
-read  PASSWORD
+read  -s PASSWORD
 
 echo "Password entered is: $PASSWORD"
