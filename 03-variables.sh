0d2165eb2c6eec95a848e99bbc24ef7aeeaf567c
ranjith2007 <ranjithkumar0799@gmail.com>
2025-09-17 20:44:26 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=Ranjith
+PERSON2=Manoj
+
+echo "$PERSON1:: HI $PERSON2, How are you?"
+echo "$PERSON2:: I'M Fine $PERSON1, How are you?"
+echo "$PERSON1:: I'M Fine, what's going on?"
+echo "$PERSON2::I'M LEARNING SHELL SCRIPT $PERSON1"
+
+
+#NOTE: 03-variables.sh passing argument from outside. so that there will be no confusion. 
