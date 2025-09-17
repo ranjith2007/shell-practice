b5d17dc5ae964df33484e4f3e32ec3410b69352d
ranjith2007 <ranjithkumar0799@gmail.com>
2025-09-17 20:29:44 +0530

    added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ranjith:: Hi Manoj, How are you?"
+echo "Manoj:: Hi Ranjith, I'm fine what about you?"
+echo "Ranjith:: Fine, what's going on?"
+echo "Manoj:: I'm practicing shell script, What about you?"
+echo "Ranjith:: I'm also practicing shell script"
