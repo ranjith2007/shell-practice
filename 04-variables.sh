204664b1271cf00c4fce5387446f7cfcd3e6b0b0
ranjith2007 <ranjithkumar0799@gmail.com>
2025-09-17 20:49:54 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1 
+PERSON2=$2
+
+echo "$1:: HI $2, How are you?"
+echo "$2:: I'M Fine $1, How are you?"
+echo "$1:: I'M Fine $2, what's going on?"
+echo "$2::I'M LEARNING SHELL SCRIPT $1"
+
+
+#$1 and $2 means passing name while executing sh 04-variables.sh Ranjith Manoj .
