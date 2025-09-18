9aefecc8b5ef71fa0f7e560b9bd27fd077f6ea06
ranjith2007 <ranjithkumar0799@gmail.com>
2025-09-18 21:27:29 +0530

    functions

--- 11-installation.sh
+++ 11-installation.sh
@@ -25,7 +25,7 @@ else
     echo "Git is already installed, nothing to do"
 fi
 
-dnf lis installed msql 
+dnf lis installed msql #MYSQL
 if [ $? -ne 0 ]
 then
     echo "Git is not installed..going to install
