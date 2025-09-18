00d26b5b682d2ffeeb8e7dbc337fe2a6d226a532
ranjith2007 <ranjithkumar0799@gmail.com>
2025-09-18 21:19:43 +0530

    variables

---
+++ 11-installation.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root priveleges"
+    exit 1
+fi
+
+dnf list installed git
+
+if [ $? -ne 0]
+then
+    echo "Git is not installed, going to install it"
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success....check it"
+        exit 1
+    else
+        echo "Git installation is success"
+    fi
+else
+    echo "Git is already installed, nothing to do"
+fi
+
+dnf lis installed msql 
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed..going to install
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Mysql installation is failure..Please check"
+        exit 1
+    else
+        echo "Mysql installation is success"
+    fi
+else
+    echo "Mysql is already installed.nothing to do"
+fi
+
